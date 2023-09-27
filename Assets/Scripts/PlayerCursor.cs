1c1b1c16e46cca2433be6b7d28196703e548c8e4
Alvina <alvinadr24@gmail.com>
2023-09-27 11:22:05 +0200

    two players system with two joystick

--- Assets/Scripts/PlayerCursor.cs
+++ Assets/Scripts/PlayerCursor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCursor : MonoBehaviour
 {
@@ -54,11 +55,15 @@ public class PlayerCursor : MonoBehaviour
 
     void Update()
     {
-        direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized;
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Shoot();
         }
+    }
+
+    public void OnMove(InputValue value)
+    {
+        direction = value.Get<Vector2>();
     }
 
     private void FixedUpdate()
