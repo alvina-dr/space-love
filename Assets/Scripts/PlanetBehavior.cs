87beee7f71bc3a105782956c1e2ec54cb3211f4e
antowarior <anto.warior@hotmail.fr>
2023-09-29 09:36:52 +0200

    led fix 2

--- Assets/Scripts/PlanetBehavior.cs
+++ Assets/Scripts/PlanetBehavior.cs
@@ -18,7 +18,10 @@ public class PlanetBehavior : MonoBehaviour
         GPSingleton.Instance.UICtrl.planetHealthBar.SetSliderValue(currentHealth, DataHolder.Instance.GeneralData.planetMaxHealth);
         serialController.SendSerialMessage(currentHealth.ToString());
         Debug.Log(currentHealth);
-        if (currentHealth <= 0) GPSingleton.Instance.GameOver();
+        if (currentHealth <= 0)
+        {
+            GPSingleton.Instance.GameOver();
+        }
     }
     #endregion
 
@@ -26,6 +29,7 @@ public class PlanetBehavior : MonoBehaviour
     void Start()
     {
         currentHealth = DataHolder.Instance.GeneralData.planetMaxHealth;
+        serialController.SendSerialMessage(currentHealth.ToString());
         GPSingleton.Instance.UICtrl.planetHealthBar.SetSliderValue(currentHealth, DataHolder.Instance.GeneralData.planetMaxHealth);
         mesh.DOLocalRotate(new Vector3(0, 180, 0), 3f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Incremental);
     }
